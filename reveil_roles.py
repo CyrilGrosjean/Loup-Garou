79872b2f21808857971571bbb31c51bc30a7b69d
Cyril Grosjean <cyril.grosjean@epitech.eu>
2020-01-21 14:56:35 +0100

    Changed renard

--- reveil_roles.py
+++ reveil_roles.py
@@ -13,3 +13,14 @@ def voyante(player_roles, options, selected):
     else:
         text = "La voyante se rendort."
     return text
+
+def renard(game_info, selected):
+    if selected != "Personne":
+        if selected in game_info.get("Loups"):
+            text = "Le renard perd son flair.\n\nLe renard se rendort."
+            game_info["RenardFlair"] = False
+        else:
+            text = "Le renard garde son flair.\n\nLe renard se rendort."
+    else:
+        text = "Le renard garde son flair.\n\nLe renard se rendort."
+    return text, game_info
